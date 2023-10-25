bfb6ebf7ee955f8082ab941a8a45064498cdbac9
makiaveli1 <itsmakiaveli@gmail.com>
2023-10-25 22:10:41 +0100

    Add more templates

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404, redirect
+from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post, Comment
 
@@ -12,15 +12,22 @@ class PostList(generic.ListView):
     
     
     
-class PostDetail(generic.ListView):
+class PostDetail(View):
 
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
 
-     def get_object(self, request, *args, **kwargs):
-         queryset = Post.objects.filter(status=1)
-         post = get_list_or_404(queryset, slug=slug)
-         comments = post.comments.filter(approved=True).order_by('created_on')
-         liked = False
-         if post.likes.filter(id=self.request.user.id).exists():
-             liked = True
-             
-         return render(request, 'post_detail.html', {'post': post, 'comments': comments, 'liked': liked})
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
